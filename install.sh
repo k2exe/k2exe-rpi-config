b280be88d3880ef3a7bc08dfb6fd38f08b1329ba
K2EXE <6319622-k2exe@users.noreply.gitlab.com>
2021-12-18 23:25:16 -0500

    ansible-lint cleanup

--- install.sh
+++ install.sh
@@ -3,8 +3,14 @@
 set -e;
 
 sudo apt-get update
-sudo apt-get -y install git ansible
+sudo apt-get -y install git python3-pip
+sudo python3 -m pip install ansible
 
 cd "$HOME"
-
-git clone https://github.com/k2exe/k2exe-rpi-config.git
+if [ -d "k2exe-rpi-config/" ]; then 
+  echo "repo exists.... checking for updates"
+  cd k2exe-rpi-config/
+  git pull origin master
+else
+  git clone https://github.com/k2exe/k2exe-rpi-config.git
+fi
